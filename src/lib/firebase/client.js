a34ff1d5c04439a0a76baa6b12ccbe81443b76ef
AlxOmar <148527399+AlxOmar04@users.noreply.github.com>
2025-08-26 19:42:06 -0500

    Agrega configuración de Firebase con keys de entorno

--- src/lib/firebase/client.js
+++ src/lib/firebase/client.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? 'AIzaSyD8RqpcvxZtQAO1IudESQLAoxmL2S4PENE',
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ?? 'sagcc-04.firebaseapp.com',
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ?? 'sagcc-04',
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ?? 'sagcc-04.appspot.com',
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ?? '362780414012',
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ?? '1:362780414012:web:7965ba41cc44b342c77339',
 }
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
